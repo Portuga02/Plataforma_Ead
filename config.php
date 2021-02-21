50da22c6418b97d26e67c884004a75279206b312
Sávio gomes da Silva <saviogomesdasilva@yahoo.com>
2021-02-20 22:14:50 -0300

    configurando bloco de exception

--- config.php
+++ config.php
@@ -2,18 +2,23 @@
 require 'environment.php';
 
 //define("BASE", "http://localhost/Plataforma_EAD/");
-define("BASE", "http://192.168.1.29/Plataforma_EAD/");
+define("BASE", "http://192.168.1.29/Plataforma_EAD/");  /*definição de seu IP*/
 
-global $config;
-$config = array();
-if (ENVIRONMENT == 'development') {
-	$config['dbname'] = 'plataforma_ead';
-	$config['host'] = 'localhost';
-	$config['dbuser'] = 'root';
-	$config['dbpass'] = '';
-} else {
-	$config['dbname'] = 'plataforma_ead';
-	$config['host'] = '192.168.1.29';
-	$config['dbuser'] = 'root';
-	$config['dbpass'] = '';
+try {
+
+	global $config;
+	$config = [];
+	if (ENVIRONMENT == 'development') {
+		$config['dbname'] = 'plataforma_ead';
+		$config['host'] = 'localhost';
+		$config['dbuser'] = 'root';
+		$config['dbpass'] = '';
+	} else {
+		$config['dbname'] = 'plataforma_ead';
+		$config['host'] = '192.168.1.29';
+		$config['dbuser'] = 'root';
+		$config['dbpass'] = '';
+	}
+} catch (Exception $e) {
+	echo 'error ao se conectar com o banco de dados' . $e->getMessage();
 }
